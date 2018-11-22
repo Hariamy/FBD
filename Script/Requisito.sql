7bab0f75abf9357786a65b56cbf5b78e0c0e3b81
Gustavo1001 <gustavomotax2@gmail.com>
2018-11-22 13:15:55 -0200

    Add files via upload

---
+++ Script/Requisito.sql
@@ -0,0 +1,10 @@
+7 - cria playlist
+- adiciona coisa na playlist
+- retorna todos os albuns
+- faixas dos albuns
+/nada sobre modificar
+/nada sobre apagar
+8 - Todos os albuns
+- acessar atributos
+- alterar os atributos
+
